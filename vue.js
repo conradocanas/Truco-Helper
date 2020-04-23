344b1b2ea5239bc8d2a4bab25017b263ca10f2a0
NazgulFS <59717278+NazgulFS@users.noreply.github.com>
2020-04-23 14:53:22 -0300

    funciones de botones

--- vue.js
+++ vue.js
@@ -5,5 +5,41 @@ const app = new Vue({
     }),
     data: () => ({
       drawer: null,
+      titulo: 'Truco Helper',
+      contEllos: 0,
+      contNos: 0
     }),
+    methods: {
+      aumentarNos(){
+        if(this.contNos >= 0 && this.contNos < 30){
+          this.contNos++
+        }else if(this.contNos < 0){
+          this.contNos = 0;
+        }else if(this.contNos >= 30){
+          this.contNos = 30;
+        }
+      },
+      aumentarEllos(){
+        if(this.contEllos >= 0 && this.contEllos < 30){
+          this.contEllos++
+        }else if(this.contEllos < 0){
+          this.contEllos = 0;
+        }else if(this.contEllos >= 30){
+          this.contEllos = 30;
+        }
+      },
+      disminuirNos(){
+        this.contNos--
+      },
+      disminuirEllos(){
+        if(this.contEllos > 0){
+          this.contEllos--
+        }
+
+      },
+      reiniciar(){
+        this.contEllos = 0;
+        this.contNos = 0;
+      }
+    },
   })
